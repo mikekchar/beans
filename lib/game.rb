d7929c9f7b631f245998de309edc5e6fcfe88acd
Mike Charlton <mikekchar@gmail.com>
2013-05-05 11:10:47 +0100

    Beans can be taken from and put into the jar

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,37 @@
 class Game
   class Hand
+    def initialize
+      @has_bean = false
+    end
+
     def empty?
-      true
+      !@has_bean
+    end
+
+    def take_bean
+      @has_bean = true
+    end
+
+    def put_bean
+      @has_bean = false
     end
   end
 
   class Jar
+    def initialize
+      @beans = 30
+    end
+
     def size
-      30
+      @beans
+    end
+
+    def take_bean
+      @beans -= 1
+    end
+
+    def put_bean
+      @beans += 1
     end
   end
 
@@ -16,6 +40,20 @@ class Game
   def initialize
     @hand = Hand.new
     @jar = Jar.new
+  end
+
+  def take_bean
+    if hand.empty?
+      jar.take_bean
+      hand.take_bean
+    end
+  end
+
+  def put_bean
+    if !hand.empty?
+      hand.put_bean
+      jar.put_bean
+    end
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,9 +11,42 @@ describe Game do
     expect(game.jar.size).to eq(30)
   end
 
-  it "can take a bean from a jar and put it in the hand" do
+  it "can take a bean from a jar" do
     game = Game.new
-    game.take
+    game.take_bean
+    expect(game.jar.size).to eq(29)
+  end
+
+  it "takes beans into the hand" do
+    game = Game.new
+    game.take_bean
     expect(game.hand).to_not be_empty
+  end
+
+  it "can not take 2 beans" do
+    game = Game.new
+    game.take_bean
+    game.take_bean
+    expect(game.jar.size).to eq(29)
+  end
+
+  it "puts beans into a jar" do
+    game = Game.new
+    game.take_bean
+    game.put_bean
+    expect(game.jar.size).to eq(30)
+  end
+
+  it "puts beans from the hand" do
+    game = Game.new
+    game.take_bean
+    game.put_bean
+    expect(game.hand).to be_empty
+  end
+
+  it "can not put beans from an empty hand" do
+    game = Game.new
+    game.put_bean
+    expect(game.jar.size).to eq(30)
   end
 end
