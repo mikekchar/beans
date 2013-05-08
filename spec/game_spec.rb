59e695785c86e62faf5ba4f57c1629f441f7dfdd
Mike Charlton <mikekchar@gmail.com>
2013-05-08 19:57:30 +0100

    Refactor game tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,30 +4,23 @@ describe Game do
   subject(:game) { Game.new }
 
   describe "the jars" do
-    it "has 5 jars" do
-      expect(game.jars.size).to eq(5)
+    it "has as many jars as bean colours" do
+      expect(game.jars.size).to eq(Game::BEAN_COLOURS.size)
     end
   end
 
   describe "the last jar" do
+
+    subject(:last_jar) { game.jars[Game::BEAN_COLOURS.size - 1]  }
+
     it "initially has 30 beans" do
-      expect(game.jars[4].size).to eq(30)
+      expect(last_jar.size).to eq(30)
     end
 
-    it "has 6 red beans" do
-      expect(game.jars[4].beans_that_are(:red).size).to eq(6)
-    end
-    it "has 6 green beans" do
-      expect(game.jars[4].beans_that_are(:green).size).to eq(6)
-    end
-    it "has 6 blue beans" do
-      expect(game.jars[4].beans_that_are(:blue).size).to eq(6)
-    end
-    it "has 6 yellow beans" do
-      expect(game.jars[4].beans_that_are(:yellow).size).to eq(6)
-    end
-    it "has 6 black beans" do
-      expect(game.jars[4].beans_that_are(:black).size).to eq(6)
+    it "has a specific number of beans for each colour" do
+      Game::BEAN_COLOURS.each do |colour|
+        expect(last_jar.beans_that_are(colour).size).to eq(Game::BEANS_PER_COLOUR)
+      end
     end
   end
 end
