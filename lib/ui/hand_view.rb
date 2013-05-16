e232512e415d35a81d7cd9aae4129ad65a13fb9e
Mike Charlton <mikekchar@gmail.com>
2013-05-16 21:55:23 +0100

    Add a hand view

---
+++ lib/ui/hand_view.rb
@@ -0,0 +1,25 @@
+module UI
+  class HandView
+
+    class HandInfo
+      attr_reader :colour
+
+      def initialize(hand)
+        if hand.empty?
+          @colour = "empty"
+        else
+          @colour = hand.bean.colour.to_s
+        end
+      end
+    end
+
+    def initialize(hand)
+      @info = HandInfo.new(hand)
+    end
+
+    def render
+      return "Hand: #{@info.colour}"
+    end
+  end
+end
+
