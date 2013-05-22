a32f84e2d75a0d78453bc951f2d6ce98461376d7
Mike Charlton <mikekchar@gmail.com>
2013-05-22 21:58:45 +0100

    Add a game controller/widget
    This is really a widget.  Checking in so I can
    easily change the names.

---
+++ lib/ui/game_controller.rb
@@ -0,0 +1,14 @@
+require "domain/game"
+require "ui/jar_view"
+require "ui/hand_view"
+
+module UI
+  class GameController
+    attr_reader :jar_view, :hand_view
+    def initialize(game)
+      @game = game
+      @jar_view = JarView.new(game.jars)
+      @hand_view =HandView.new(game.hand)
+    end
+  end
+end
--- spec/ui/hand_view_spec.rb
+++ spec/ui/hand_view_spec.rb
@@ -3,21 +3,19 @@ require 'domain/hand'
 require 'domain/jar'
 require 'domain/bean'
 
-module UI
-  describe HandView do
-    let(:hand) { Hand.new }
-    let(:hand_view) { UI::HandView.new(hand) }
-    let(:jar) { Jar.new([Bean.new(:red)]) }
+describe UI::HandView do
+  let(:hand) { Hand.new }
+  let(:hand_view) { UI::HandView.new(hand) }
+  let(:jar) { Jar.new([Bean.new(:red)]) }
 
-    it "shows an empty hand" do
-      expect(hand).to be_empty
-      expect(hand_view.render).to eq("Hand: empty")
-    end
+  it "shows an empty hand" do
+    expect(hand).to be_empty
+    expect(hand_view.render).to eq("Hand: empty")
+  end
 
-    it "shows the correct bean" do
-      hand.take_bean_from(jar)
-      expect(hand).to_not be_empty
-      expect(hand_view.render).to eq("Hand: red")
-    end
+  it "shows the correct bean" do
+    hand.take_bean_from(jar)
+    expect(hand).to_not be_empty
+    expect(hand_view.render).to eq("Hand: red")
   end
 end
